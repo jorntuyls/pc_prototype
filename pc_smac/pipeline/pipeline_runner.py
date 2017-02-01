6148149bbe0531c980e368e2f25126f67e747d66
Jorn Tuyls <jorntuyls@Jorns-MacBook-Pro-3.local>
2017-02-01 11:00:02 +0100

    Correct standard pipeline runner

--- pc_smac/pipeline/pipeline_runner.py
+++ pc_smac/pipeline/pipeline_runner.py
@@ -27,11 +27,7 @@ class PipelineRunner(object):
         self.statistics = statistics
         self.pipeline_builder = PipelineBuilder(pipeline_space, caching=False, cache_directory=None)
 
-    def run(self, config,
-                    instance=None,
-                    seed=None,
-                    cutoff=np.inf,
-                    instance_specific=None):
+    def run(self, config, instance, seed):
         """
             See ExecuteTARun class in SMAC3: https://github.com/automl/SMAC3
             Parameters
@@ -60,7 +56,7 @@ class PipelineRunner(object):
         """
 
         print("start tae_runner")
-        print(config, instance, cutoff, seed, instance_specific)
+        print(config, instance, seed)
         # start timer
         start_timer = time.time()
 
@@ -97,25 +93,17 @@ class PipelineRunner(object):
                 prec_score = precision_score(y_valid, y_pred, average='macro')
                 print("SCORE: {}".format(prec_score))
                 scores.append(prec_score)
-            score = np.mean(scores)
+            cost = 1 - np.mean(scores)
         except ValueError as v:
             exc_info = sys.exc_info()
-            status = StatusType.SUCCESS
-            score = 0
+            cost = 1234567890
             # Display the *original* exception
             traceback.print_exception(*exc_info)
             del exc_info
 
         # Calculate score and total runtime
-        cost = 1 - score
         runtime = time.time() - start_timer
         print("cost: {}, time: {}, status: {}".format(cost, runtime, status))
-
-        # Update runhistory
-        self.runhistory.add(config=config,
-                            cost=cost, time=runtime, status=status,
-                            instance_id=instance, seed=seed,
-                            additional_info=additional_info)
 
         # Add information of this run to statistics
         run_information = {
@@ -126,7 +114,7 @@ class PipelineRunner(object):
         self.statistics.add_run(config.get_dictionary(), run_information)
 
         print("stop tae_runner")
-        return status, cost, runtime, additional_info
+        return cost, additional_info
 
     def add_runtime_timing(self, dct, timing):
         for key in timing.keys():
@@ -234,100 +222,7 @@ class CachedPipelineRunner(PipelineRunner):
         self.statistics.add_run(config.get_dictionary(), run_information)
 
         print("stop cached tae_runner")
-        return float(cost), additional_info
-
-    # def start(self, config,
-    #                 instance=None,
-    #                 seed=None,
-    #                 cutoff=np.inf,
-    #                 instance_specific=None):
-    #
-    #     print("start cached tae_runner")
-    #     print(config, instance, cutoff, seed, instance_specific)
-    #     # start timer
-    #     start_timer = time.time()
-    #
-    #     self.runtime_timing = {}
-    #     additional_info = {}
-    #     status = StatusType.SUCCESS
-    #     score = 0
-    #
-    #     pipeline = self.pipeline_builder.build_pipeline(config)
-    #
-    #     # Cross validation as in scikit-learn:
-    #     #   http://scikit-learn.org/stable/tutorial/statistical_inference/model_selection.html
-    #     K = 3
-    #     X_folds = np.array_split(self.X_train, K)
-    #     y_folds = np.array_split(self.y_train, K)
-    #     scores = list()
-    #     try:
-    #         for k in range(0, K):
-    #             # Fit pipeline
-    #             X_train = list(X_folds)
-    #             X_valid = X_train.pop(k)
-    #             X_train = np.concatenate(X_train)
-    #             y_train = list(y_folds)
-    #             y_valid = y_train.pop(k)
-    #             y_train = np.concatenate(y_train)
-    #             pipeline.fit(X_train, y_train)
-    #
-    #             # Keep track of timing information
-    #             self.add_runtime_timing(self.transformer_runtime_timing, pipeline.pipeline_info.get_preprocessor_timing())
-    #             self.add_runtime_timing(self.runtime_timing, pipeline.pipeline_info.get_timing_flat())
-    #             print("TIMING: {}".format(pipeline.pipeline_info.get_timing()))
-    #
-    #             # Validate pipeline
-    #             score_start = time.time()
-    #             # TODO Does it make sense to cache the validation too? Or doesn't this take much time?
-    #             y_pred = pipeline.predict(X_valid)
-    #             prec_score = precision_score(y_valid, y_pred, average='macro')
-    #             score_time = time.time() - score_start
-    #             print("TIME: {}, SCORE: {}".format(score_time, prec_score))
-    #             scores.append(prec_score)
-    #         score = np.mean(scores)
-    #     except ValueError as v:
-    #         exc_info = sys.exc_info()
-    #         status = StatusType.SUCCESS
-    #         score = 0
-    #         # Display the *original* exception
-    #         traceback.print_exception(*exc_info)
-    #         del exc_info
-    #
-    #     # Update cache hits
-    #     self.cache_hits['total'] += pipeline.pipeline_info.get_cache_hits()[0]
-    #     self.cache_hits['cache_hits'] += pipeline.pipeline_info.get_cache_hits()[1]
-    #
-    #     # Get reduction in runtime for cached configuration if it was not already cached
-    #     # TODO insert this
-    #     # if pipeline.pipeline_info.get_cache_hits()[1] == 0:
-    #     t_rc = self._get_pipeline_steps_timing(self.runtime_timing, config)
-    #     additional_info['t_rc'] = t_rc
-    #
-    #     # Calculate score and total runtime
-    #     cost = 1 - score
-    #     runtime = time.time() - start_timer
-    #     print("cost: {}, time: {}, status: {}".format(cost, runtime, status))
-    #     print("Total function evaluations: {}, cache hits: {}".format(self.cache_hits['total'],
-    #                                                                   self.cache_hits['cache_hits']))
-    #
-    #     # Update runhistory
-    #     self.runhistory.add(config=config,
-    #                         cost=cost, time=runtime, status=status,
-    #                         instance_id=instance, seed=seed,
-    #                         additional_info=additional_info)
-    #
-    #     # Add information of this run to statistics
-    #     run_information = {
-    #         'cost': cost,
-    #         'runtime': runtime,
-    #         'pipeline_steps_timing':  self.runtime_timing,
-    #         'cache_hits': self.cache_hits['cache_hits'],
-    #         'total_evaluations': self.cache_hits['total']
-    #     }
-    #     self.statistics.add_run(config.get_dictionary(), run_information)
-    #
-    #     print("stop cached tae_runner")
-    #     return status, cost, runtime, additional_info
+        return cost, additional_info
 
     def clean_cache(self):
         self.pipeline_builder.clean_cache()
