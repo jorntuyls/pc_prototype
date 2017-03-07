1884c3b598c94d2475bf5055254db27b35eaebd7
Jorn Tuyls <jorntuyls@Jorns-MacBook-Pro-3.local>
2017-03-07 14:29:46 +0100

    Remove unused code

--- pc_smac/experiments/run_whitebox_random_search_experiment.sh
+++ pc_smac/experiments/run_whitebox_random_search_experiment.sh
@@ -1,2 +1,2 @@
 cd /Users/jorntuyls/Documents/workspaces/thesis
-python -m pc_smac.pc_smac.experiments.run_whitebox_random_search_experiment -l=20 -w=200 -n=20 -t=paraboloid -x 0.25 0.75 -y 0.5 0.5 -o=/Users/jorntuyls/Documents/workspaces/thesis/pc_smac/output/
+python -m pc_smac.pc_smac.experiments.run_whitebox_random_search_experiment -l=12 -w=4000 -n=20 -t=beale -x 0.1 0.9 -y 0.25 0.25 -o=/Users/jorntuyls/Documents/workspaces/thesis/pc_smac/output/
