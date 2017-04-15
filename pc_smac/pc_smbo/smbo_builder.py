2e54e86ef3e76f1485040a6c7caf0994956ea6ec
Jorn Tuyls <jorntuyls@Jorns-MBP-3.home>
2017-04-15 08:48:45 +0100

    marginalisation parameters in method init and not in run function

--- pc_smac/pc_smbo/smbo_builder.py
+++ pc_smac/pc_smbo/smbo_builder.py
@@ -90,7 +90,9 @@ class SMBOBuilder:
                                                                           acquisition_func=acq_func_wrapper,
                                                                           rng=rng,
                                                                           constant_pipeline_steps=constant_pipeline_steps,
-                                                                          variable_pipeline_steps=variable_pipeline_steps)
+                                                                          variable_pipeline_steps=variable_pipeline_steps,
+                                                                          num_marginalized_configurations_by_random_search=10,
+                                                                          num_configs_for_marginalization=50)
         elif acq_func_name == 'eips':
             acquisition_func = EIPS(model)
             acq_func_wrapper = PCAquisitionFunctionWrapper(acquisition_func=acquisition_func,
@@ -132,7 +134,9 @@ class SMBOBuilder:
                                                                           acquisition_func=acq_func_wrapper,
                                                                           rng=rng,
                                                                           constant_pipeline_steps=constant_pipeline_steps,
-                                                                          variable_pipeline_steps=variable_pipeline_steps)
+                                                                          variable_pipeline_steps=variable_pipeline_steps,
+                                                                          num_marginalized_configurations_by_random_search=10,
+                                                                          num_configs_for_marginalization=50)
         elif acq_func_name == 'pceips':
             acquisition_func = PCEIPS(model)
             acq_func_wrapper = PCAquisitionFunctionWrapperWithCachingReduction(acquisition_func=acquisition_func,
