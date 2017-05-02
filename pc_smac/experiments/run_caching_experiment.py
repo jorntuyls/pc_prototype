7fcb1b1cabd254529a0c618b65bc2d4593e4371f
Jorn Tuyls <jorntuyls@Jorns-MacBook-Pro-3.local>
2017-05-02 21:28:49 +0200

    Cache timing experiment

--- pc_smac/experiments/run_caching_experiment.py
+++ pc_smac/experiments/run_caching_experiment.py
@@ -1,13 +1,66 @@
+
+import os
 import argparse
 import time
+import json
 
 from sklearn.externals import joblib
 
 from pc_smac.pc_smac.data_loader.data_loader import DataLoader
+from pc_smac.pc_smac.pipeline_space.feature_preprocessing_nodes.extra_rand_trees import ExtraTreesNode
+from pc_smac.pc_smac.pipeline_space.feature_preprocessing_nodes.fast_ica import FastICANode
+from pc_smac.pc_smac.pipeline_space.feature_preprocessing_nodes.feature_agglomeration import FeatureAgglomerationNode
 from pc_smac.pc_smac.pipeline_space.feature_preprocessing_nodes.kernel_pca import KernelPcaNode
+from pc_smac.pc_smac.pipeline_space.feature_preprocessing_nodes.kitchen_sinks import RandomKitchenSinksNode
+from pc_smac.pc_smac.pipeline_space.feature_preprocessing_nodes.linear_svm import LinearSVMNode
+from pc_smac.pc_smac.pipeline_space.feature_preprocessing_nodes.no_preprocessing import NoPreprocessingNode
+from pc_smac.pc_smac.pipeline_space.feature_preprocessing_nodes.nystroem_sampler import NystroemSamplerNode
+from pc_smac.pc_smac.pipeline_space.feature_preprocessing_nodes.pca import PcaNode
+from pc_smac.pc_smac.pipeline_space.feature_preprocessing_nodes.polynomial import PolynomialFeaturesNode
+from pc_smac.pc_smac.pipeline_space.feature_preprocessing_nodes.random_trees_embedding import RandomTreesEmbeddingNode
+from pc_smac.pc_smac.pipeline_space.feature_preprocessing_nodes.select_percentile import SelectPercentileNode
+from pc_smac.pc_smac.pipeline_space.feature_preprocessing_nodes.select_rates import SelectRatesNode
 
 
-def run_caching_experiment(data_path):
+def run_caching_experiment(stamp, data_path, data_set_id, prepr_name, cache_dir=None, output_dir=None):
+    data_path = os.path.join(data_path, data_set_id)
+
+    if cache_dir == None:
+        cache_dir = os.path.dirname(os.path.abspath(__file__))
+    if output_dir == None:
+        output_dir = os.path.dirname(os.path.abspath(__file__))
+
+    cache_dir = os.path.join(cache_dir, data_set_id + "/" + prepr_name)
+    output_dir = os.path.join(output_dir, data_set_id + "/" + prepr_name)
+
+    try:
+        if not os.path.exists(cache_dir):
+            os.makedirs(cache_dir)
+    except FileExistsError:
+        pass
+
+    try:
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+    except FileExistsError:
+        pass
+
+    preprocessor_nodes = {
+        'extra_rand_trees': ExtraTreesNode(),
+        'fast_ica': FastICANode(),
+        'feature_agglomeration': FeatureAgglomerationNode(),
+        'kernel_pca': KernelPcaNode(),
+        'kitchen_sinks': RandomKitchenSinksNode(),
+        'linear_svm': LinearSVMNode(),
+        'no_preprocessing': NoPreprocessingNode(),
+        'nystroem_sampler': NystroemSamplerNode(),
+        'pca': PcaNode(),
+        'polynomial_features': PolynomialFeaturesNode(),
+        'rand_trees_embedding': RandomTreesEmbeddingNode(),
+        'select_percentile': SelectPercentileNode(),
+        'select_rates': SelectRatesNode()
+    }
+
     data_loader = DataLoader(data_path)
     data = data_loader.get_data()
 
@@ -15,65 +68,94 @@ def run_caching_experiment(data_path):
     y_train = data["y_train"]
     print(X_train.shape, y_train.shape)
 
-    # K = 2
-    # X_folds = np.array_split(X_train, K)
-    # y_folds = np.array_split(y_train, K)
-    #
-    # X_train_1 = list(X_folds)
-    # X_train_2 = X_train_1.pop(1)
-    # X_train_1 = np.concatenate(X_train_1)
-    # y_train_1 = list(y_folds)
-    # y_train_2 = y_train_1.pop(1)
-    # y_train_1 = np.concatenate(y_train_1)
 
-    #### CACHE transformer ####
-    fp_node = KernelPcaNode()
+    output = dict()
+
+
+    fp_node = preprocessor_nodes[prepr_name]
     transformer_name, transformer = fp_node.initialize_algorithm(hyperparameters={})
+    print(transformer)
     print("Start")
-    print(transformer_name, transformer)
     start_time = time.time()
     transformer_after = transformer.fit(X_train, y_train)
-    print("Timing transform: {}".format(time.time() - start_time))
-    fp_node = KernelPcaNode()
+    timing = time.time() - start_time
+    print("Timing transform: {}".format(timing))
+
+    fp_node = preprocessor_nodes[prepr_name]
+    transformer_name, transformer = fp_node.initialize_algorithm(hyperparameters={})
+    print(transformer)
+    print("Start")
+    start_time = time.time()
+    transformer_after = transformer.fit(X_train, y_train)
+    timing = time.time() - start_time
+    print("Timing transform: {}".format(timing))
+    output['fit_timing_alone'] = timing
+
+    #### CACHE transformer ####
+    fp_node = preprocessor_nodes[prepr_name]
     transformer_name_2, transformer_2 = fp_node.initialize_algorithm(hyperparameters={})
     print("Start")
     print(transformer_name_2, transformer_2)
     start_time = time.time()
     transformer_after = transformer_2.fit(X_train, y_train)
-    joblib.dump(transformer_after, '/Users/jorntuyls/Desktop/transform.pkl')
-    print("Timing transform and persist: {}".format(time.time() - start_time))
+    joblib.dump(transformer_after, os.path.join(cache_dir, stamp + '_transform.pkl'))
+    timing = time.time() - start_time
+    print("Timing transform and persist: {}".format(timing))
+    output['cache_transformer_fit_timing_with_cache'] = timing
 
-    start_time = time.time()
+
     print("Start")
-    transformer_after = joblib.load('/Users/jorntuyls/Desktop/transform.pkl')
-    print("    Load time: {}".format(time.time() - start_time))
+    start_time = time.time()
+    transformer_after = joblib.load(os.path.join(cache_dir, stamp + '_transform.pkl'))
+    load_timing = time.time() - start_time
     X1 = transformer_after.transform(X_train)
-    print("Timing load transformer and transform : {}".format(time.time() - start_time))
+    timing = time.time() - start_time
+    print("Timing load transformer and transform : {}".format(timing))
+    output['cache_transformer_load_cache'] = load_timing
+    output['cache_transformer_load_cache_and_transform'] = timing
 
     #### CACHE result ####
 
-    fp_node = KernelPcaNode()
-    transformer_name_2, transformer_2 = fp_node.initialize_algorithm(hyperparameters={})
+    fp_node = preprocessor_nodes[prepr_name]
+    transformer_name_3, transformer_3 = fp_node.initialize_algorithm(hyperparameters={})
     print("Start")
-    print(transformer_name_2, transformer_2)
+    print(transformer_name_3, transformer_3)
     start_time = time.time()
-    X = transformer_2.fit(X_train, y_train).transform(X_train)
-    joblib.dump(X, '/Users/jorntuyls/Desktop/X.pkl')
-    print("Timing transform and persist result: {}".format(time.time() - start_time))
+    X = transformer_3.fit(X_train, y_train).transform(X_train)
+    joblib.dump(X, os.path.join(cache_dir, stamp + '_X.pkl'))
+    timing = time.time() - start_time
+    print("Timing transform and persist result: {}".format(timing))
+    output['cache_result_fit_timing_with_cache'] = timing
 
-    start_time = time.time()
     print("Start")
-    X2 = joblib.load('/Users/jorntuyls/Desktop/X.pkl')
-    print("Timing load result and transform : {}".format(time.time() - start_time))
+    start_time = time.time()
+    X2 = joblib.load(os.path.join(cache_dir, stamp + '_X.pkl'))
+    timing = time.time() - start_time
+    print("Timing load result and transform : {}".format(timing))
+    output['cache_result_load_cache'] = timing
+
+    save_json([output], os.path.join(output_dir, 'result_' + stamp + '.json'))
 
     print(X1 == X2)
 
+def save_json(lst, destination_file):
+    if not os.path.exists(destination_file):
+        open_file(destination_file)
 
+    with open(destination_file, "a") as fp:
+        for row in lst:
+            json.dump(row, fp, indent=4, sort_keys=True)
+            fp.write("\n")
+
+def open_file(file):
+    f = open(file, 'w')
+    f.close()
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("-l", "--location", type=str, help="Dataset directory")
-    parser.add_argument("-s", "--seed", type=int, default=None, help="Seed for sampling configurations")
+    parser.add_argument("-d", "--dataid", type=str, help="Dataset directory")
+    parser.add_argument("-s", "--stamp", type=str, default="", help="Stamp")
     parser.add_argument("-p", "--pname", type=str, default=None, help="Preprocessor name")
     parser.add_argument("-o", "--outputdir", type=str, default=None, help="Output directory")
     parser.add_argument("-ds", "--downsampling", type=int, default=None,
@@ -85,5 +167,10 @@ def parse_arguments():
 
 if __name__ == "__main__":
     args = parse_arguments()
-    run_caching_experiment(data_path=args.location)
+    run_caching_experiment(stamp=args.stamp,
+                           data_path=args.location,
+                           data_set_id=args.dataid,
+                           prepr_name=args.pname,
+                           cache_dir=args.cachedir,
+                           output_dir=args.outputdir)
 
